4d959b41b5cdd72dde924e512b7ebf4a3d4f6f8e
bbenlolo <bbenlolo@eaest.com>
2024-04-15 16:15:44 -0400

    Updated script to start identifying words beginning with b

--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,21 @@
-const content = document.querySelector("p");
-console.log(content.id);
+// const content = document.querySelector("p");
 
-if (content) {
-  const img = document.createElement("img");
-  img.src = chrome.runtime.getURL("images/image0.jpg");
-  content.insertAdjacentElement("afterend", img);
-}
+// if (content) {
+//   const img = document.createElement("img");
+//   img.src = chrome.runtime.getURL("images/image0.jpg");
+//   content.insertAdjacentElement("afterend", img);
+// }
+
+const textContent = document.body.innerHTML;
+const alteredArray = [];
+textContent.split(" ").forEach((word) => {
+  if (word.charAt(0).toLocaleLowerCase == 'b') {
+    word = "Brendan";
+    console.log("here");
+  }
+  alteredArray.push(word);
+});
+console.log(alteredArray);
+const alteredText = alteredArray.join(" ");
+document.body.innerHTML = alteredText
+console.log(alteredText);
